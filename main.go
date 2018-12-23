a67b82592f11edac26c75e52459ee8d125f9631d
Dan Luhring <dan+github@luhrings.com>
2018-12-23 12:43:09 -0500

    Improve help text

--- main.go
+++ main.go
@@ -20,11 +20,17 @@ func main() {
 
 	if numberOfArgumentsPassedIn == 0 {
 		displayHelpText()
+		os.Exit(0)
 	}
 
 	if numberOfArgumentsPassedIn == 1 {
 		if os.Args[1] == "--restore" || os.Args[1] == "-r" {
 			restorePermanentCredentials()
+			os.Exit(0)
+		}
+
+		if os.Args[1] == "--help" || os.Args[1] == "-h" {
+			displayHelpText()
 			os.Exit(0)
 		}
 
@@ -39,14 +45,30 @@ func exitWithFormattedErrorMessage(format string, a ...interface{}) {
 }
 
 func displayHelpText() {
-	const helpText = `To obtain temporary session credentials from AWS and save to credentials file:
-awsmfa <mfa-token>
+	const helpText = `Syntax: awsmfa [commands] [mfa-token]
 
-To restore original AWS credentials to credentials file:
-awsmfa --restore
+Commands:
+
+-h, --help          Show this help text
+-r, --restore       Restore original credentials back to AWS credentials file
+
+'mfa-token' must be the currently displayed numeric MFA token from the device you've configured as a virtual MFA device associated with your IAM user. In addition, active IAM access credentials must already have been stored in your local 'credentials' file or in the AWS-specific environment variables. For help with enabling a virtual MFA device, see https://docs.aws.amazon.com/IAM/latest/UserGuide/id_credentials_mfa_enable_virtual.html.
+
+Examples:
+
+To obtain temporary session credentials from AWS and save to credentials file:
+
+$ awsmfa 123456
+[You can now perform AWS actions that require MFA...]
+
+To switch back to using permanent access credentials:
+
+$ awsmfa --restore
+[You can no longer perform AWS actions that require MFA...]
+
+For more information: https://github.com/luhring/awsmfa
 `
 	fmt.Println(helpText)
-	os.Exit(0)
 }
 
 func restorePermanentCredentials() {
